6b0ffce0807b52548ef46b82a69dffc939002ed2
RMP <ruthiepina@gmail.com>
2023-11-28 00:10:46 -0700

    Completed authorization code.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -21,7 +21,6 @@ const generateRandomString = (length) => {
    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
 
-const codeVerifier = generateRandomString(128);
 
 //* Code Challenge - from Spotify for Developers
 //* Once the code verifier has been generated, we must transform (hash) it using the SHA256 algorithm.
@@ -30,29 +29,34 @@ const sha256 = async (plain) => {
     const encoder = new TextEncoder()
     const data = encoder.encode(plain)
     return window.crypto.subtle.digest('SHA-256', data)
-  }
+}
 
 //* Function base64encode - from Spotify for Developers
 //* Returns the base64 representation of the digest we just calculated with the sha256 function  
 const base64encode = (input) => {
     return btoa(String.fromCharCode(...new Uint8Array(input)))
-      .replace(/=/g, '')
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_');
-  }
+    .replace(/=/g, '')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_');
+}
 
 //* Code challenge generation - from Spotify for Developers
+const codeVerifier = generateRandomString(128);
 const hashed = await sha256(codeVerifier)
 const codeChallenge = base64encode(hashed);
 
 //* Request User Authorization - from Spotify for Developers
-const clientId = 'YOUR_CLIENT_ID';
+//* The app generates a PKCE code challenge and redirects to the Spotify authorization server login page by updating the window.location object value. 
+//* This allows the user to grant permissions to our application
+//* Please note that the code verifier value is stored locally using the localStorage JavaScript property for use in the next step of the authorization flow.
+
+const clientId = '09492227f96b49f889b2baa58716b1a3';
 const redirectUri = 'http://localhost:8080';
 
 const scope = 'user-read-private user-read-email';
 const authUrl = new URL("https://accounts.spotify.com/authorize")
 
-// generated in the previous step
+//* generated in the previous step
 window.localStorage.setItem('code_verifier', codeVerifier);
 
 const params =  {
@@ -71,5 +75,4 @@ window.location.href = authUrl.toString();
 
 
 
-var genreList = [];
-var previousList = []; //* is it necessary to show last shown list of tracks?
+
